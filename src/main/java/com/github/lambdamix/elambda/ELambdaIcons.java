2e2ff3cbb6d2349e2224155c7d19dfdbefc24a27
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-12-11 18:54:22 +0000

    lambda.png

--- src/main/java/com/github/lambdamix/elambda/ELambdaIcons.java
+++ src/main/java/com/github/lambdamix/elambda/ELambdaIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class ELambdaIcons {
-    public static final Icon FILE = IconLoader.getIcon("/icons/lambda.jpg", ELambdaIcons.class);
+    public static final Icon FILE = IconLoader.getIcon("/icons/lambda.png", ELambdaIcons.class);
 }
