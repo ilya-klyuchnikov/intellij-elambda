1f75123b1d936a8a55ade08f74923800a9fdfd05
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-12-11 18:23:40 +0000

    parsing: snapshot testing

---
+++ src/test/java/com/github/lambdamix/elambda/ELambdaParsingTest.java
@@ -0,0 +1,26 @@
+package com.github.lambdamix.elambda;
+
+import com.intellij.testFramework.ParsingTestCase;
+
+public class ELambdaParsingTest extends ParsingTestCase {
+    public ELambdaParsingTest() {
+        super("", "elambda", new ELambdaParserDefinition());
+    }
+
+    public void testSimple() {
+        doTest(true);
+    }
+
+    public void testErr01() {
+        doTest(true);
+    }
+
+    public void testErr02() {
+        doTest(true);
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return "src/test/testData/parsing";
+    }
+}
